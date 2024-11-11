7e8e1215253ecfd9f1c3eee134ca8e9f147d47dc
Blightsower <xg2id2d4qr5yk1d6xg@gmail.com>
2024-11-10 17:54:53 -0700

    changes not reflecting

--- Knight Jump/scripts/interactibles/collectibles/coin.gd
+++
@@ -1,8 +0,0 @@
-extends Area2D
-
-@onready var game_manager: Node = %GameManager
-@onready var animation_player: AnimationPlayer = $AnimationPlayer
-
-func _on_body_entered(body: Node2D) -> void:
-	GameManager.increase_score(1)
-	animation_player.play("pickup")
