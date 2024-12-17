abb37d39a3dfaba0a94f40b70c3c8a72dcafba39
Blightsower <xg2id2d4qr5yk1d6xg@gmail.com>
2024-12-16 19:35:51 -0700

    added event bus pattern

---
+++ Knight Jump/scripts/singleton/events.gd
@@ -0,0 +1,21 @@
+extends Node
+
+# player events
+signal on_player_life_changed(new_value:int)
+signal on_player_health_changed(new_value:int)
+signal on_player_death()
+signal on_player_spawn()
+
+# stage events
+signal on_game_start()
+signal on_stage_finish()
+signal on_stage_checkpoint()
+
+# menu events
+signal on_pause_menu()
+signal on_input_menu()
+signal on_option_menu()
+
+# entity events
+signal on_coin_collect(new_value:int)
+signal on_monster_die()
