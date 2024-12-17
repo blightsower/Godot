08587d09f0054164b497d5e665b76b44207faf88
Blightsower <xg2id2d4qr5yk1d6xg@gmail.com>
2024-12-16 19:38:27 -0700

    now implements events.gd

--- Knight Jump/scripts/user interface/hud.gd
+++ Knight Jump/scripts/user interface/hud.gd
@@ -4,13 +4,14 @@ extends CanvasLayer
 @onready var score_label: Label = $ScoreControl/ScoreLabel
 
 func _on_ready() -> void:
-	GameManager.on_life_changed.connect(on_life_changed)
-	GameManager.on_score_increased.connect(on_score_changed)
-	life_label.text = "x" + str(GameManager.get_life())
-	score_label.text = "x" + str(GameManager.get_score())
+	Events.on_player_life_changed.connect(on_life_changed)
+	Events.on_coin_collect.connect(on_score_changed)
 	
-func on_life_changed(val):
+	life_label.text = "x" + str(GameManager.life)
+	score_label.text = "x" + str(GameManager.score)
+	
+func on_life_changed(val:int):
 	life_label.text = "x" + str(val)
 
-func on_score_changed(val):
+func on_score_changed(val:int):
 	score_label.text = "x" + str(val)
